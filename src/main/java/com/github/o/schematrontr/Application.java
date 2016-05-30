5570a920477aefe0b6e9a04ce8fc317901f4875a
Philip Helger <philip@helger.com>
2016-05-30 14:41:54 +0200

    Switched to Pure Schematron

--- src/main/java/com/github/o/schematrontr/Application.java
+++ src/main/java/com/github/o/schematrontr/Application.java
@@ -1,13 +1,14 @@
 package com.github.o.schematrontr;
 
 import com.helger.commons.io.resource.ClassPathResource;
-import com.helger.schematron.xslt.SchematronResourceXSLT;
+import com.helger.schematron.pure.SchematronResourcePure;
 
-public class Application {
+public class Application
+{
 
-
-    public static void main(String[] args) {
-        new SchematronResourceXSLT(new ClassPathResource("xslt/UBL-TR_Main_Schematron.xslt")).isValidSchematron();
-    }
+  public static void main (final String [] args)
+  {
+    new SchematronResourcePure (new ClassPathResource ("schematron/UBL-TR_Main_Schematron.xml")).isValidSchematron ();
+  }
 
 }
